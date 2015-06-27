042d6336a50383d12f3c556033ab88bc1c6b6efe
k.Kitada <1127ways@gmail.com>
2015-06-27 16:55:27 +0900

    shopのcrud作成

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,22 +1,48 @@
 class ShopsController < ApplicationController
   def index
+    @shop = Shop.all
   end
 
   def new
+    @shop = Shop.new
   end
 
   def create
+    @shop = Shop.new(shop_params)
+    @shop.save
+    redirect_to @shop
   end
 
   def edit
+    @shop = Shop.find(params[:id])
   end
 
   def update
+    @shop = Shop.find(params[:id])
+    shop.update(shop_params)
+    redirect_to @shop
   end
 
   def show
+    @shop = Shop.find(params[:id])
   end
 
   def destroy
+    @shop = Shop.find(params[:id])
+    @shop.destroy
+    redirect_to @shop
   end
+
+private
+
+ def shop_params
+  params.require(:shop).permit(
+    :shop_name,
+    :tel,
+    :address,
+    :url,
+    )
 end
+
+end
+
