8af2556b3bf9903b1af5fe584cc2d2fdd844c679
k.Kitada <1127ways@gmail.com>
2015-06-27 15:19:06 +0900

    店舗情報migrate

--- db/migrate/20150627060038_create_shops.rb
+++ db/migrate/20150627060038_create_shops.rb
@@ -1,12 +1,13 @@
 class CreateShops < ActiveRecord::Migration
   def change
     create_table :shops do |t|
-      t.string :shop_name
-      t.integer :tel
-      t.string :address
-      t.text :url
+      t.string :shop_name,null: false
+      t.integer :tel, null: false
+      t.string :address, null: false
+      t.text :url, null: false
 
       t.timestamps null: false
     end
+    add_index :shops, :url, unique: true
   end
 end
